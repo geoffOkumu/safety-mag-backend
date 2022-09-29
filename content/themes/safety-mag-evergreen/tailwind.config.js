70a58447b3a24dd8daf1e1667a9a506922fce7d3
Geoffrey Okumu <geoffocum@gmail.com>
2022-09-29 11:50:48 +0000

    direct import theme

---
+++ content/themes/safety-mag-evergreen/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.hbs", "./**/*.hbs"],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        fontFamily: {
+            sans: ["Merriweather Sans", "sans-serif"],
+            serif: ["Merriweather", "serif"],
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
+};
